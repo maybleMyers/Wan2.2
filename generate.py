86738d3e7cbe0fe44e3dbcc2ca908c3215fe9635
maybleMyers <benjimon678@yahoo.com>
2025-09-20 12:01:28 -0700

    fix import

--- generate.py
+++ generate.py
@@ -17,7 +17,7 @@ from PIL import Image
 
 import wan
 try:
-    from RamTorch.ramtorch.modules import Linear as RamTorchLinear
+    from ramtorch_src.ramtorch.modules import linear as RamTorchLinear
     print("Successfully imported RamTorch.")
 except ImportError:
     RamTorchLinear = None
